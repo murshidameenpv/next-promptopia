014bfaefc31e3a691cd4a0d1eed849b0ef4e0d4e
murshidameen <murshidmonzpv@gmail.com>
2024-05-21 14:17:25 +0530

    navigation setup on mobile and desktop

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,25 @@
 import '@styles/global.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+export const metadata = {
+  title: "Promptopia",
+  description:"Tutorial"
+}
 
-const layout = () => {
+const RootLayout = ({children}) => {
   return (
-    <div>layout</div>
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient'/>
+        </div>
+        <main className='app'>
+          <Nav/>
+         {children}
+        </main>
+      </body>
+   </html>
   )
 }
 
-export default layout
+export default RootLayout;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
-const page = () => {
+import Feed from '@components/Feed';
+const Home = () => {
   return (
-    <div>page</div>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br/>
+        <span className="orange_gradient text-center">AI-Powered Prompts </span>
+      </h1>
+      <p className='desc text-center'>Promptopia is  an Open-Source AI Prompting Tool For Modern World To Discover , Create And Share Creative Prompts</p>
+      <Feed/>
+    </section>
+  );
 }
 
-export default page
+export default Home
