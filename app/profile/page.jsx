c87f4e3737d05adaba5802fbb135c5b0947c8209
murshidameen <murshidmonzpv@gmail.com>
2024-05-31 11:37:28 +0530

    delete  prompt fixed

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -20,7 +20,20 @@ const MyProfile = () => {
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`)
   };
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(`Are you sure you want delete this Prompt?`);
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method:'DELETE'
+        })
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+        setPosts(filteredPosts)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  };
   return (
     <Profile
       name="My"
